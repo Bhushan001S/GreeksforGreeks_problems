59370c4bbc033ada7a89995d8dfdfe83e84609a6
Bhushan001S <102133759+Bhushan001S@users.noreply.github.com>
2025-01-16 21:17:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Largest subarray of 0's and 1's/largest-subarray-of-0s-and-1s.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int maxLen(vector<int> &arr) {
+        
+        int sum=0,maxi=0;
+        unordered_map<int,int>mp;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]==0) sum+=-1;
+            else sum+=1;
+            
+            if(sum==0) maxi=i+1;
+            
+            if(mp.find(sum)!=mp.end()) {
+                maxi=max(maxi,i-mp[sum]);
+            }else mp[sum]=i;
+            
+        }
+        return maxi;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int T;
+    cin >> T;
+    cin.ignore(); // To ignore the newline character after reading T
+
+    while (T--) {
+        string line;
+        getline(cin, line); // Read the whole line for the array
+
+        // Convert the line into an array of integers
+        stringstream ss(line);
+        vector<int> a;
+        int num;
+        while (ss >> num) {
+            a.push_back(num);
+        }
+
+        // Create the solution object
+        Solution obj;
+
+        // Call the maxLen function and print the result
+        cout << obj.maxLen(a) << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends
